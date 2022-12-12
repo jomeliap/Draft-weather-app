6c8efd349cdc0a75a12e265056c22e74abf897d7
jomeliap <jomeliap@gmail.com>
2022-12-12 02:44:16 +0100

    Updated the API integration
    included the time and date descriptions on js and html

---
+++ src/index.js
@@ -0,0 +1,36 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = date.getDay();
+  let daysWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = daysWeek[days];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}`;
+  let humidity = response.data.main.humidity;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${humidity}`;
+  let wind = Math.round(response.data.wind.speed);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${wind}`;
+  document.querySelector("#date-description").innerHTML =
+    response.data.weather[0].main;
+}
+
+let apiKey = "a4da89de5958761ca307fe3393bc406e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
